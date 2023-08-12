c48559e87328babfc913d9fd3bd13fa7a1cde0cd
joshi-omkar <joshiomkar0400@gmail.com>
2023-08-12 22:09:28 +0530

    department page

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './departments.css'
+import { useNavigate } from 'react-router-dom';
+import { useInventory } from '../contexts/InventoryContext';
 
 const Departments = () => {
 
+  const navigate = useNavigate()
+  const { data, inventory, dispatch } = useInventory();
+
     const Card = ({ title }) => {
+        const handleOnClick = () => {
+          dispatch({
+            type: "FILTER_DEPARTMENT",
+            department: title,
+            inventoryData: data,
+          });
+          navigate('/products')
+        }
         return (
-          <div className="departments-card">
+          <div onClick={handleOnClick} className="departments-card">
             <h2>{title}</h2>
           </div>
         );
